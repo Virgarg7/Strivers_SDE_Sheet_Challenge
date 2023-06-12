b6f52b9f95d3c48d7ee4d1b6f80bd0169901a748
Vir garg <98869884+Virgarg7@users.noreply.github.com>
2023-06-12 23:15:56 +0530

    Create Ceil_BST.cpp

---
+++ Ceil_BST.cpp
@@ -0,0 +1,28 @@
+
+int findCeil(BinaryTreeNode<int> *node, int input){
+    // Smallest value of maxi which is >= input
+    int maxi = -1;
+
+    // Special case when all nodes are lesser than input , then there will be no ciel
+
+    BinaryTreeNode<int> *temp = node;
+
+    while (temp != NULL)
+    {
+        if (temp->data == input)
+        {
+            return input;
+        }
+        else if (temp->data > input)
+        {
+            maxi = temp->data;
+            temp = temp->left;
+        }
+        else
+        {
+            temp = temp->right;
+        }
+    }
+
+    return maxi;
+}
